971e84f5770649bebdde7eb5d517004355c88ad7
Austin Amani <amaniaustin123@gmail.com>
2024-04-07 09:04:01 +0300

    Finished setting up Authentication

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html'],
+	content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
 	theme: {
 		fontFamily: {
 			ubuntu: 'Ubuntu, sans-serif',
