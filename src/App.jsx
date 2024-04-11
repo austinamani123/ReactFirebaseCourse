681d63fea81c8cb402c42a26903071f460c33c88
Austin Amani <45035115+austinamani123@users.noreply.github.com>
2024-04-11 10:12:05 +0300

    Worked on the Create part of CRUD (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,50 @@ import React from'react'
 import { Auth } from './components/auth'
 import { db } from './config/firebase'
 import { useState, useEffect } from 'react'
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, addDoc, collection } from 'firebase/firestore'
 
 function App() {
 
     const [movieList, setMovieList] = useState([])
 
+    const [newMovieTitle, setNewMovieTitle] = useState('')
+    const [newReleaseDate, setNewReleaseDate] = useState(0)
+    const [isNewMovieOscar, setIsNewMovieOscar] = useState(false)
+
     const moviesCollectionRef = collection(db, 'movies')
 
-    useEffect(() => {
-        const getMovieList = async () => {
-            try{
-                const data = await getDocs(moviesCollectionRef)
-                const filteredData = data.docs.map((doc) => ({
-                    ...doc.data(), 
-                    id: doc.id,
-                }))
-                console.log(filteredData)
-                setMovieList(filteredData)
-            } catch (err){
-                console.log(err)
-            }
+    const getMovieList = async () => {
+        try{
+            const data = await getDocs(moviesCollectionRef)
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(), 
+                id: doc.id,
+            }))
+            console.log(filteredData)
+            setMovieList(filteredData)
+        } catch (err){
+            console.log(err)
         }
+    }
 
+    useEffect(() => {
         getMovieList()
     },[])
+
+    const onSubmitMovie = async () => {
+        try {
+            await addDoc(moviesCollectionRef, {
+                title: newMovieTitle, 
+                releaseDate: newReleaseDate,
+                receivedAnOscar: isNewMovieOscar,
+            })
+
+            getMovieList()
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
     
 
     return(
@@ -34,7 +53,21 @@ function App() {
             <Auth />
 
             <div>
-                
+               <input 
+                    placeholder='Movie title...'
+                    onChange={(e) => setNewMovieTitle(e.target.value)}
+                /> 
+               <input 
+                    placeholder='Release date...' 
+                    type='number' 
+                    onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+                />
+               <input 
+                    type="checkbox" 
+                    checked={isNewMovieOscar} 
+                    onChange={(e) => setIsNewMovieOscar(e.target.checked)}/> 
+               <label>Receieved an Oscar</label>
+               <button onClick={onSubmitMovie}>Submit Movie</button>
             </div>
 
 
