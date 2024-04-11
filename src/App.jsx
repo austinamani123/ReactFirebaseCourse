c904660e34ed4849a84652ed61f5e2e236424a0b
Austin Amani <45035115+austinamani123@users.noreply.github.com>
2024-04-11 09:37:53 +0300

    Filtered the movie data and displayed it (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import React from'react'
 import { Auth } from './components/auth'
+import { db } from './config/firebase'
+import { useState, useEffect } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
 
 function App() {
+
+    const [movieList, setMovieList] = useState([])
+
+    const moviesCollectionRef = collection(db, 'movies')
+
+    useEffect(() => {
+        const getMovieList = async () => {
+            try{
+                const data = await getDocs(moviesCollectionRef)
+                const filteredData = data.docs.map((doc) => ({
+                    ...doc.data(), 
+                    id: doc.id,
+                }))
+                console.log(filteredData)
+                setMovieList(filteredData)
+            } catch (err){
+                console.log(err)
+            }
+        }
+
+        getMovieList()
+    },[])
+    
+
     return(
         <div className="App">
             <Auth />
+
+            <div>
+                
+            </div>
+
+
+            <div>
+                {movieList.map((movie) => (
+                  <div>
+                    <h1 style={{color: movie.receivedAnOscar ? 'green' : 'red'}}> {movie.title} </h1>
+                    <p> {movie.releaseDate} </p>
+                  </div>  
+                ))}
+            </div>
         </div>
     )
 }
